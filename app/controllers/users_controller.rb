7112e543fcd5c9a861010198ef67a204c4af7384
jendobes <jdobak1@gmail.com>
2018-03-23 18:17:39 +0100

    added basic sign up and login functionalty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    @user = User.create(username: params[:name], password: params[:password])
+    @user.save
+    login(@user.id)
+    redirect to "/user/#{@user.id}"
+  end
+
+  get '/user/:id' do
+    @user = User.find(params[:id])
+    erb :'/users/show_user'
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        login(user.id)
+        redirect to "/user/#{user.id}"
+      else
+        redirect to '/signup'
+      end
+  end
+
 end
