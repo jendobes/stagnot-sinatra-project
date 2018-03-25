bd3c7819c00441f50e1487a653d29530b1c08641
jendobes <jdobak1@gmail.com>
2018-03-25 20:36:50 +0200

    added activity index and show pages

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,5 +3,21 @@ class TodosController < ApplicationController
   get '/activities' do
     erb :'/todos/index'
   end
-  
+
+  get '/activities/:id' do
+    @activity = Todo.find(params[:id])
+    erb :'/todos/show'
+  end
+
+  get '/activities/add/:id' do
+    if logged_in?
+      @user = current_user
+      @activity = Todo.find(params[:id])
+      Wishlist.create(:user_id => current_user.id, :todo_id => activity.id)
+      erb :'/todos/addwishlist'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
