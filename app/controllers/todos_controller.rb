d59417c469aa1cd644ae0279018773964cce9341
jendobes <jdobak1@gmail.com>
2018-03-25 23:20:58 +0200

    added links between pages, added page to create a new activity

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,6 +4,14 @@ class TodosController < ApplicationController
     erb :'/todos/index'
   end
 
+  get '/activities/new' do
+    if logged_in?
+      erb :'/todos/new'
+    else
+      redirect to "/login"
+    end
+  end
+
   get '/activities/:id' do
     @activity = Todo.find(params[:id])
     erb :'/todos/show'
@@ -13,8 +21,13 @@ class TodosController < ApplicationController
     if logged_in?
       @user = current_user
       @activity = Todo.find(params[:id])
-      Wishlist.create(:user_id => current_user.id, :todo_id => activity.id)
+      array = User.find(2).wishlists.map{|a| a.todo.id}
+      if !array.include?(@activity.id)
+      Wishlist.create(:user_id => current_user.id, :todo_id => @activity.id)
       erb :'/todos/addwishlist'
+    else
+      erb :'/todos/addfail'
+    end
     else
       redirect to '/login'
     end
