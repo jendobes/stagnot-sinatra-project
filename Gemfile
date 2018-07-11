b0a2084ed608d4918f798b0b2778c83a115ea107
jendobes <jdobak1@gmail.com>
2018-07-11 19:18:09 -0400

    prep app for heroku deployment

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ gem 'activerecord', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
 gem 'require_all'
-gem 'sqlite3'
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
@@ -17,4 +16,12 @@ group :test do
   gem 'capybara'
   gem 'rack-test'
   gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
+end
+
+group :development do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg', '< 1.0'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,18 @@
-ENV['SINATRA_ENV'] ||= "development"
-
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
 
 require_all 'app'
+require 'bundler/setup'
+require 'active_record'
+
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+configure :development do
+    ENV['SINATRA_ENV'] ||= "development"
+    ActiveRecord::Base.establish_connection(
+      :adapter => "sqlite3",
+      :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+    )
+end
+
+configure :production do
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+end
